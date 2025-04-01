1bc24235787f9516be52a00af01454d081a90568
Delta <delta@deltawolf.net>
2025-04-01 00:18:20 -0700

    Use AttachAudioClip

--- ResoniteFish/ResoniteFish.cs
+++ ResoniteFish/ResoniteFish.cs
@@ -1,6 +1,4 @@
-
 using System;
-
 using Elements.Core;
 using FrooxEngine;
 using HarmonyLib;
@@ -13,35 +11,34 @@ public class ResoniteFish : ResoniteMod {
 	public override string Author => "Delta & Raidriar";
 	public override string Version => VERSION_CONSTANT;
 	public override string Link => "https://github.com/XDelta/ResoniteFish";
-	public static ModConfiguration? Config;
 
-	[AutoRegisterConfigKey] public static readonly ModConfigurationKey<bool> FishSound =
-        new ModConfigurationKey<bool>("FishSound", "Enable Sounds", () => false);
+	[AutoRegisterConfigKey]
+	public static readonly ModConfigurationKey<bool> FishSound = new("FishSound", "Enable Sounds", () => false);
+
+	public static ModConfiguration? Config;
 
 	public override void OnEngineInit() {
 		Harmony harmony = new("net.deltawolf.Fish");
 		harmony.PatchAll();
 		Config = GetConfiguration()!;
-        Config!.Save(true);
+		Config!.Save(true);
 	}
 
 	[HarmonyPatch(typeof(InteractionHandler), "OpenContextMenu")]
-	private class ContextMenuOpenMenuAPatch {
+	private class ContextMenuOpenMenuPatch {
 		public static void Postfix(InteractionHandler __instance, InteractionHandler.MenuOptions options) {
 			ContextMenu ctx = __instance.ContextMenu;
 			if (options == InteractionHandler.MenuOptions.Default) {
-				var Button = ctx.AddItem("Fish", new Uri("resdb:///5a56714e4cc021888cb51162cc34b55a7a1333fe8bc162c8be906f9345206b3c.png"), colorX.Cyan);
+				ContextMenuItem Button = ctx.AddItem("Fish", new Uri("resdb:///5a56714e4cc021888cb51162cc34b55a7a1333fe8bc162c8be906f9345206b3c.png"), colorX.Cyan);
 
 				if (Config!.GetValue(FishSound) == true) {
-					var MenuSlot = Button.Slot;
-
-					var StaticAudio = MenuSlot.AttachComponent<StaticAudioClip>();
-					StaticAudio.URL.Value = new Uri("resdb:///1ed74a85e7d9a8e2456fadbd276cb9dd223ad96377ad230409693d408524606e.wav");
+					Slot MenuSlot = Button.Slot;
+					var StaticAudio = MenuSlot.AttachAudioClip(new Uri("resdb:///1ed74a85e7d9a8e2456fadbd276cb9dd223ad96377ad230409693d408524606e.wav"));
 
 					var ClipPlayer = MenuSlot.AttachComponent<RandomAudioClipPlayer>();
 					ClipPlayer.Clips.Add();
 					ClipPlayer.Clips.GetElement(0).Clip.Target = StaticAudio;
-					
+
 					var Trigger = MenuSlot.AttachComponent<ButtonActionTrigger>();
 					Trigger.OnPressed.Target = ClipPlayer.Play;
 				}
