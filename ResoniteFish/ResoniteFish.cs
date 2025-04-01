d650012237bd4d3d8009842834ff94ae73da1175
Raidriar796 <145519075+Raidriar796@users.noreply.github.com>
2025-03-31 23:24:12 -0700

    Fish, but with sound

--- ResoniteFish/ResoniteFish.cs
+++ ResoniteFish/ResoniteFish.cs
@@ -24,7 +24,19 @@ public class ResoniteFish : ResoniteMod {
 		public static void Postfix(InteractionHandler __instance, InteractionHandler.MenuOptions options) {
 			ContextMenu ctx = __instance.ContextMenu;
 			if (options == InteractionHandler.MenuOptions.Default) {
-				ctx.AddItem("Fish", new Uri("resdb:///5a56714e4cc021888cb51162cc34b55a7a1333fe8bc162c8be906f9345206b3c.png"), colorX.Cyan);
+				var Button = ctx.AddItem("Fish", new Uri("resdb:///5a56714e4cc021888cb51162cc34b55a7a1333fe8bc162c8be906f9345206b3c.png"), colorX.Cyan);
+
+				var MenuSlot = Button.Slot;
+
+				var StaticAudio = MenuSlot.AttachComponent<StaticAudioClip>();
+				StaticAudio.URL.Value = new Uri("resdb:///1ed74a85e7d9a8e2456fadbd276cb9dd223ad96377ad230409693d408524606e.wav");
+
+				var ClipPlayer = MenuSlot.AttachComponent<RandomAudioClipPlayer>();
+				ClipPlayer.Clips.Add();
+				ClipPlayer.Clips.GetElement(0).Clip.Target = StaticAudio;
+
+				var Trigger = MenuSlot.AttachComponent<ButtonActionTrigger>();
+				Trigger.OnPressed.Target = ClipPlayer.Play;
 			}
 		}
 	}
